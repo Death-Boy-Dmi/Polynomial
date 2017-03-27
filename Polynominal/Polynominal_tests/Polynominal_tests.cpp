3516c5b74373e003edac6b6b8b74e2e29fcd0347
Dmitry Smertin <smertindmi@mail.ru>
2017-03-27 13:23:44 +0300

    2 теста

--- Polynominal/Polynominal_tests/Polynominal_tests.cpp
+++ Polynominal/Polynominal_tests/Polynominal_tests.cpp
@@ -6,12 +6,31 @@ TEST(CPolynom, can_use_default_constructor)
 {
 	ASSERT_NO_THROW(CPolynom pol());
 }
+TEST(CPolynom, can_use_main_constructor)
+{
+	string strP = "-3x^2y + 4x^3y^4z^1";
+	string strV = "x y z";
+	static unsigned int power = 10;
 
+	ASSERT_NO_THROW(CPolynom pol(strP, strV, power));
+}
 TEST(CPolynom, can_use_Calculate)
 {
 	string strP = "-3x^2y + 4x^3y^4z^1";
 	string strV = "x y z";
-	static unsigned int p = 10;
-	CPolynom pol(strP, strV, p);
+	static unsigned int power = 10;
+	CPolynom pol(strP, strV, power);
+
 	ASSERT_NO_THROW(pol.Calculate());
 }
+
+TEST(CPolynom, can_use_ToString)
+{
+	string strP = "-3x^2y + 4x^3y^4z^1";
+	string strV = "x y z";
+	static unsigned int power = 10;
+	CPolynom pol(strP, strV, power);
+
+	ASSERT_NO_THROW(pol.ToString());
+}
+
