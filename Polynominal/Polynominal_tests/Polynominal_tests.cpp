bb40264d7a54c6d98ddcf987f3c07908cd4d79bb
Dmitry Smertin <smertindmi@mail.ru>
2017-04-02 17:01:50 +0300

    Снова ошибка линковщика

--- Polynominal/Polynominal_tests/Polynominal_tests.cpp
+++ Polynominal/Polynominal_tests/Polynominal_tests.cpp
@@ -2,7 +2,14 @@
 
 #include <gtest.h>
 
+unsigned int CPolynom::Power = 10;
+
 TEST(CPolynom, can_use_construct)
 {
 	ASSERT_NO_THROW(CPolynom pol());
+}
+
+TEST(CPolynom, can_use_main_construct)
+{
+	ASSERT_NO_THROW(CPolynom pol("x y", "x^2y^3+3x^3y^4"));
 }
