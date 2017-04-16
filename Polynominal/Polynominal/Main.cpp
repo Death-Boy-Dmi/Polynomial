63d87ffa80bbd59ec5c0544916e94c4b96e0cea4
Dmitry Smertin <smertindmi@mail.ru>
2017-04-16 15:56:10 +0300

    Заключительное исправление

--- Polynominal/Polynominal/Main.cpp
+++ Polynominal/Polynominal/Main.cpp
@@ -6,36 +6,37 @@ string* CPolynom::arrVar;
 
 void main()
 {
-	//string strPol;
-	//string strVar;
-	//double res;
+	string strPol;
+	string strVar;
+	double res;
 
-	//cout << "Enter the names of variabels as a priority (through a space):\n\n\t";
-	//getline (cin,strVar);
-	//cout << "\n";
-	//
-	//cout << "Enter the max power:\n\n\t";
-	//cin >> CPolynom::Power;
-	//cout << "\n";
-	//
-	//cout << "Enter the polynom:\n\n\t";
-	//cin >> strPol;
-	//cout << "\n";
-	//
-	//CPolynom polinom(strPol, strVar);
-	//
-	//polinom.StreamSetVar();
-	//res = polinom.Calculate();
-	//
-	//cout << "\tresult = " << res << endl;
-	//
-	//cout << "\n\n\t" << polinom.ToString() << endl;
+	cout << "Enter the names of variabels as a priority (through a space):\n\n\t";
+	getline (cin,strVar);
+	cout << "\n";
 	
-	CPolynom::Power = 10;
-	CPolynom pol1("-3x^2y^3+3x^3y^2", "x y");
-	CPolynom pol2("-3x^2y^3+3x^3y^2", "x y");
-	CPolynom pol = pol1 + pol2;
+	cout << "Enter the max power:\n\n\t";
+	cin >> CPolynom::Power;
+	cout << "\n";
+	
+	cout << "Enter the polynom:\n\n\t";
+	cin >> strPol;
+	cout << "\n";
+	
+	CPolynom polinom(strPol, strVar);
+	
+	polinom.StreamSetVar();
+	res = polinom.Calculate();
+	
+	cout << "\tresult = " << res << endl;
+	
+	cout << "\n\n\t" << polinom.ToString() << endl;
+	
+	//CPolynom::Power = 10;
+	//CPolynom pol1("-3x^2y^3+3x^3y^2", "x y");
+	//CPolynom pol2("-3x^2y^3+3x^3y^2", "x y");
+	//CPolynom pol = pol1 + pol2;
 
-	cout << "\n\n\t" << pol.ToString()<< endl;
+	//cout << "\n\n\t" << pol.ToString()<< endl;
+
 	system("PAUSE");
 }
