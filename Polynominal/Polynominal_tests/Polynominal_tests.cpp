481552d7102563a948ef5fca1edfc0504a4313aa
Dmitry Smertin <smertindmi@mail.ru>
2017-05-21 23:45:24 +0300

    fix test

--- Polynominal/Polynominal_tests/Polynominal_tests.cpp
+++ Polynominal/Polynominal_tests/Polynominal_tests.cpp
@@ -3,8 +3,7 @@
 #include <gtest.h>
 
 unsigned int CPolynom::Power = 10;
-unsigned int CPolynom::numVar;
-string* CPolynom::arrVar;
+double* CPolynom::arrArg;
 
 TEST(CPolynom, can_use_construct)
 {
@@ -13,12 +12,12 @@ TEST(CPolynom, can_use_construct)
 
 TEST(CPolynom, can_use_main_construct)
 {
-	ASSERT_NO_THROW(CPolynom pol("-3x^2y^3+3x^3y^2", "x y"));
+	ASSERT_NO_THROW(CPolynom pol("-3x^2y^3+3x^3y^2"));
 }
 
 TEST(CPolynom, can_use_Calculate)
 {
-	CPolynom pol("-3x^2y^3+3x^3y^2", "x y");
+	CPolynom pol("-3x^2y^3+3x^3y^2");
 	double arr[2] = { 3,10 };
 	pol.SetVar(arr);
 
@@ -27,7 +26,7 @@ TEST(CPolynom, can_use_Calculate)
 
 TEST(CPolynom, can_correct_use_Calculate)
 {
-	CPolynom pol("-3x^2y^3+3x^3y^2", "x y");
+	CPolynom pol("-3x^2y^3+3x^3y^2");
 	double arr[2] = { 3,10 };
 	pol.SetVar(arr);
 	double res = pol.Calculate();
@@ -37,14 +36,14 @@ TEST(CPolynom, can_correct_use_Calculate)
 
 TEST(CPolynom, can_use_ToString)
 {
-	CPolynom pol("-3x^2y^3+3x^3y^2", "x y");
+	CPolynom pol("-3x^2y^3+3x^3y^2");
 
 	ASSERT_NO_THROW(pol.ToString());
 }
 
 TEST(CPolynom, can_correct_use_ToString)
 {
-	CPolynom pol("-3x^2y^3+3x^3y^2", "x y");
+	CPolynom pol("-3x^2y^3+3x^3y^2");
 	string str = pol.ToString();
 
 	EXPECT_EQ(str, "-3.000000*x^2y^3+3.000000*x^3y^2");
@@ -52,16 +51,16 @@ TEST(CPolynom, can_correct_use_ToString)
 
 TEST(CPolynom, can_use_Plus)
 {
-	CPolynom pol1("-3x^2y^3+3x^3y^2", "x y");
-	CPolynom pol2("-3x^2y^3+3x^3y^2", "x y");
+	CPolynom pol1("-3x^2y^3+3x^3y^2");
+	CPolynom pol2("-3x^2y^3+3x^3y^2");
 
 	ASSERT_NO_THROW(pol1 + pol2);
 }
 
 TEST(CPolynom, can_correct_use_Plus)
 {
-	CPolynom pol1("-3x^2y^3+3x^3y^2", "x y");
-	CPolynom pol2("-3x^2y^3+3x^3y^2", "x y");
+	CPolynom pol1("-3x^2y^3+3x^3y^2");
+	CPolynom pol2("-3x^2y^3+3x^3y^2");
 	CPolynom pol = pol1 + pol2;
 
 	EXPECT_EQ(pol.ToString(), "-6.000000*x^2y^3+6.000000*x^3y^2");
@@ -69,14 +68,14 @@ TEST(CPolynom, can_correct_use_Plus)
 
 TEST(CPolynom, can_use_Mult)
 {
-	CPolynom pol1("-3x^2y^3+3x^3y^2", "x y");
+	CPolynom pol1("-3x^2y^3+3x^3y^2");
 
 	ASSERT_NO_THROW(pol1*3);
 }
 
 TEST(CPolynom, can_correct_use_Mult)
 {
-	CPolynom pol1("-3x^2y^3+3x^3y^2", "x y");
+	CPolynom pol1("-3x^2y^3+3x^3y^2");
 	CPolynom pol = pol1*2;
 
 	EXPECT_EQ(pol.ToString(), "-6.000000*x^2y^3+6.000000*x^3y^2");
